9e30304ab5a8ed405b6c6033e258d0d347458361
SecondBeta <94583891+SecondBeta@users.noreply.github.com>
2022-04-20 00:49:29 -0400

    Law project update
    I'm not proud of this one lol

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,5 +1,11 @@
 const projectsData = [
   {
+    title: 'Law Project',
+    description: `Debating the positives and negatives of marijuana's legalization`,
+    imgSrc: '/static/images/marijuana.webp',
+    href: '/blog/anti-legalization-of-marijuana',
+  },
+  {
     title: 'English Project',
     description: `Call to Action: "To Educate people on the history of the Indigenous people"`,
     imgSrc: '/static/images/english.jpg',
